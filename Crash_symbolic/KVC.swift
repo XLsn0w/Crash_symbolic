69d21e6063ef0412a4ec746b156d238238cd3b9c
xlsn0w <xlsn0wios@gmail.com>
2018-07-11 13:20:45 +0800

    add

--- Crash_symbolic/KVC.swift
+++ Crash_symbolic/KVC.swift
@@ -1,14 +1,54 @@
-//
-//  KVC.swift
-//  Crash_symbolic
-//
-//  Created by HL on 2018/7/11.
-//  Copyright © 2018年 XL. All rights reserved.
-//
+/*那么，第一个问题，我们要做的是什么？
+通俗点说就是我们需要做上哪些新功能，达到哪些目标。
+这是我在动手之前思考的一个问题，先明确需求，要做哪些新功能，有目标的去找解决方案。
+这方面我列了几条
 
+解决类型不同的转换问题
+解决泛型Array的赋值问题（Array<XXModel>）
+父类和父类的父类赋值问题，往前递归几层？是否做限制？
+过滤Foundation框架里的属性
+明确了问题之后就可以找解决方案了，首先在OC下由于有强大牛逼的runtime和完善的Foundation的API，这几个问题都不是问题，不过还是记录一下吧：
+
+类型不同的转换问题：解决这个问题要单独把不同类型拉出来，如果是数字类型转string，可以直接取stringValue，也可以直接stringWithFormat，如果是string转数字类型，则可以统一用NSNumberFormatter处理，转成NSNumber即可
+泛型Array的赋值问题，runtime可以上场了，先获取当前class的所有属性，拿到objc_property_t类型，property_getAttributes这个API拿到的字符串中是包含NSArray<XXXModel>这种泛型字段的，直接把NSArray的元素类型截出来，再转换成Class对象就可以了。
+父类的问题，这里我思考了很久，网上几个库的解决方案不一样，有的是直接递归到NSObject层，再在属性筛选的时候把系统API给去掉，有的是在递归的时候加个判断，递归到Foundation的时候就break，我比较了一下觉得后者性能更好，减少了递归次数，但是没有实验的地方是如果子类override了Foundation框架中的属性会怎么样，不过这么做的情况也很少，我也就暂时忽略掉了。
+Foundation属性的过滤，这个在上一层父类递归的时候就已经筛选掉了，因为不会递归到Foundation的类上面去，自然也不会出现Foundation的属性。
+刚才说的是OC的方案，也就是使用runtime的方案，那么在swift下这个方案可不可行呢？
+我实验了一下，Swift中的原生Array在property_getAttributes这个可以获取泛型的API拿到的类型是NSArray，同时NSArray在这个API下拿到的也是NSArray，区分不开，好了，那我就抛弃runtime找找其他的解决方案吧。
+
+首先找到的库是Reflection，这个库很吊很牛逼，牛逼到我现在还没完全看懂它的代码（当然是我太菜了），里面大概的思路是拿到Swift下面的类的C指针，然后对指针去做偏移，从而动态拿到这个类的属性的一系列信息，这个库我看了下确实满足了我所有需求，但是秉持着作死的精神，我还是想自己找到一个看得懂的解决方案出来。
+
+第二个库是GrandModel，这个整体的方案和我之前的方案大致相似：通过Mirror动态拿属性列表，拿完之后用NSObject的setValue(_:forKey:)去做赋值，那看来我的方案是可以实现我的需求的！那就这么做了！
+
+由于加入了Swift的原生类型，所以不能用Foundation框架里的API去做了，我想到的方案是Mirror的subjectType可以拿到当前类的类型，那么我就需要遍历当前类的所有property，对每个value取mirror，去拿类型再存起来。
+
+类型转换问题，数字类型转String，直接走"\(value)"，这个简单，string转数字这个需要对每个数字类型进行判断，如果是Int要调Int的init，如果是Double要调double的init，这个还有优化空间。
+泛型Array的处理，通过Mirror拿到的subjectType也是包含元素类型的信息的，由于swift的所有Array和Dictionary必须要注明元素类型，所以我这里要做一个分支处理：如果Array的元素类型是Model，而且实际从服务端传入的value也是[String:Any]的情况下才进行Array的遍历KVC，如果上面的条件不满足，那么就要判断类Array的元素类型和服务端value的元素类型是否一致，如果不一致直接赋值会有crash风险，核心代码如下：
+if valueType.hasPrefix("Array") {
+    let arrayClassName = valueType.substring(with: valueType.index(valueType.startIndex, offsetBy: 6)..<valueType.index(valueType.endIndex, offsetBy: -1));
+    if arrayClassName.hasPrefix("Dictionary<String") {
+        if property.arrayClass != nil && property.arrayClass == .item {
+            ret = type(of: self).itemsWithArray(value as! Array<Dictionary<String, Any>>);
+        } else  {
+            //arrayClass不是item，无需作数组泛型KVC，直接赋值，此处有因为Dictionary的泛型不一致导致的crash风险，待处理
+            ret = value;
+        }
+    } else {
+        ret = value;
+    }
+}
+父类的处理，我这里直接抛弃了Swift原生类型的继承的处理，我觉得自己继承Array去写一个类的情况也比较少，就简单处理了，沿用上面OC的处理就行了，只是OC取父类是用runtime的API，Swift里我用的是superMirror。
+Foundation属性的过滤，这一条和OC处理方法一致，在父类递归取属性的时候，如果父类的类型以NS开头，就直接break就行了。
+
+*/
 import UIKit
 
-class Item: NSObject {
+class Item: NSObject {///这是因为KVC是OC 的方法，OC方法和属性都需要运行时写到类对象中，swift 利用KVC设置属性需要运行时机制，swift4.0以前，编译器默认帮我们做了在对象声明前加上了@objc，4.0需要手动加上。
+    
+    //MARK : 存储属性
+    @objc var  access_token:String = ""
+    @objc var  expires_in:TimeInterval = 0.0
+    @objc var  uid:String?
     
     func autoKVCBinding(_ dictionary:Dictionary<String, Any>?) {///单纯只做了空值校验，property如果还是同名model的话可以做递归的KVC校验。
         if dictionary != nil {
@@ -28,12 +68,6 @@ class Item: NSObject {
     
 }
 
-class KVC: UIView {
-    
-
-    
-
-    
 
     /*
      在Swift中使用KVC分为3中情况:
@@ -84,4 +118,3 @@ class KVC: UIView {
      
     */
 
-}
--- Crash_symbolic/atos.swift
+++ Crash_symbolic/atos.swift
@@ -8,3 +8,236 @@
 
 
 ///atos实际是一个可以把地址转换为函数名（包括行号）的工具，atos 语法：atos -o dysm文件路径 -l 模块load地址 调用方法的地址，上面的0x182274000就是模块load地，0x0000000182399900就是调用方法的地址。
+
+/*
+一、场景
+客户端的开发流程都相似，如android，搞ios开发就要不停地发版本，随之而来的就是各种版本的崩溃日志（称为crash log）。如果不能好好地管理，那么开发人员很快就会在crash log和版本的海洋里迷失方向。解决崩溃问题是移动应用开发者最日常的工作之一。如果是开发过程中遇到的崩溃，可以根据重现步骤调试，但线上版本就无能为力了。
+
+国内，一般的公司是没有对ios crash符号化分析系统的，能够对ios 的crash日志做符号化做得好的公司比较少，比较好的是 “腾讯bugly”，链接：http://bugly.qq.com 。ios的日志分析，做符号化，并定位到发生崩溃的原因，对app的开发百利无一害，功德无量。
+
+关注问题本身，客户端开发和后台服务开发管理日志有很大的不同：
+
+如果是服务器报了个异常，对于java的服务，RD登上机器，看看错误的日志，看到“crash by...” 找出原因，改代码，重新编译打包部署一下就算修复了，假如是分布式的集群，服务是可以不间断的提供服务的。
+
+如果是客户端开发，在实际的项目开发中，崩溃问题，依赖xcode编辑器，依赖于系统记录的崩溃日志或错误堆栈，在本地开发调试阶段，是没有问题的。如果在发布的线上版本出现崩溃问题，开发者是无法即时准确的取得错误堆栈的。需要适当的时期将crash上报到服务端，由服务端处理收集分析。客户端需要重新发版才能修复旧版的crash。
+
+
+ demo 原始crash日志：
+ 
+
+ 
+ ### 1.进程信息 ###
+ Incident Identifier: 87164E05-84A8-40F2-886D-14F90C9D3F47
+ CrashReporter Key:   TODO
+ Hardware Model:      iPhone7,2
+ Process:         imeituan [945]
+ Path:            /var/mobile/Containers/Bundle/Application/96DC918D-60C8-4C49-A51D-C0D71D0FCB4D/imeituan.app/imeituan
+ Identifier:      com.meituan.imeituan
+ Version:         895
+ Code Type:       ARM-64
+ Parent Process:  ??? [1]
+ 
+ ### 2.基本信息 ###
+ Date/Time:       2016-02-01 16:14:24 +0000
+ OS Version:      iPhone OS 9.2.1 (13D15)
+ Report Version:  104
+ 
+ ### 3.异常信息 ###
+ Exception Type:  SIGABRT
+ Exception Codes: #0 at 0x181f3c140
+ 
+ ### 4.线程回溯 ###
+ Crashed Thread:  0
+ Application Specific Information:
+ *** Terminating app due to uncaught exception 'NSRangeException', reason: '*** -[__NSArrayM objectAtIndex:]: index 25 beyond bounds [0 .. 24]'
+ 
+ ### 5.Crash调用堆栈，需要需要将堆栈的二进制转成可读的###
+ Last Exception Backtrace:
+ 0   CoreFoundation                      0x0000000182399900 0x182274000 + 1202432
+ 1   libobjc.A.dylib                     0x0000000181a07f80 0x181a00000 + 32640
+ 2   CoreFoundation                      0x000000018227f828 0x182274000 + 47144
+ 3   imeituan                            0x0000000101ab593c 0x1000b8000 + 27253052
+ 4   imeituan                            0x0000000100521a68 0x1000b8000 + 4627048
+ 5   UIKit                               0x00000001873dd31c 0x187078000 + 3560220
+ 6   UIKit                               0x00000001873dd484 0x187078000 + 3560580
+ 7   UIKit                               0x00000001873cc7e8 0x187078000 + 3491816
+ 8   UIKit                               0x00000001873e1fb0 0x187078000 + 3579824
+ 9   UIKit                               0x000000018717708c 0x187078000 + 1044620
+ 10  UIKit                               0x0000000187087778 0x187078000 + 63352
+ 11  QuartzCore                          0x0000000184a96b2c 0x184a88000 + 60204
+ 12  QuartzCore                          0x0000000184a91738 0x184a88000 + 38712
+ 13  QuartzCore                          0x0000000184a915f8 0x184a88000 + 38392
+ 14  QuartzCore                          0x0000000184a90c94 0x184a88000 + 35988
+ 15  QuartzCore                          0x0000000184a909dc 0x184a88000 + 35292
+ 16  QuartzCore                          0x0000000184a8a0cc 0x184a88000 + 8396
+ 17  CoreFoundation                      0x0000000182350588 0x182274000 + 902536
+ 18  CoreFoundation                      0x000000018234e32c 0x182274000 + 893740
+ 19  CoreFoundation                      0x000000018234e75c 0x182274000 + 894812
+ 20  CoreFoundation                      0x000000018227d680 0x182274000 + 38528
+ 21  GraphicsServices                    0x000000018378c088 0x183780000 + 49288
+ 22  UIKit                               0x00000001870f4d90 0x187078000 + 511376
+ 23  imeituan                            0x0000000100112628 0x1000b8000 + 370216
+ 24  ???                                 0x0000000181e1e8b8 0x0 + 0
+ 
+ …
+ 
+ ### 6.动态库信息 ###
+ Binary Images:
+ 0x1000b8000 -        0x10275ffff +imeituan arm64  <e38d01be571931b7a4c3d9dcbf28e821> /var/mobile/Containers/Bundle/Application/96DC918D-60C8-4C49-A51D-C0D71D0FCB4D/imeituan.app/imeituan
+ 0x10d76c000 -        0x10d7dbfff  AGXMetalG4P arm64  <f76b11f8d06338f99ae4704aba08111c> /System/Library/Extensions/AGXMetalG4P.bundle/AGXMetalG4P
+ 0x182058000 -        0x18225dfff  libicucore.A.dylib arm64  <5c1540546de5350ab314c1d4c8a46d1b> /usr/lib/libicucore.A.dylib
+ 
+ …
+ 
+ 0x182274000 -        0x1825ecfff  CoreFoundation arm64  <121118a9a44d3518b99f3ebfd8806f69> /System/Library/Frameworks/CoreFoundation.framework/CoreFoundation
+ 0x1825f0000 -
+ 
+
+ /// handle
+ 主要有6部分组成:
+ 
+ 1.进程信息
+ 2.基本信息
+ 3.异常信息
+ 4.线程回溯
+ 5.Crash调用堆栈（全是地址信息，需要使用符号表转成可读的）
+ 6.动态库信息（第5部分依赖的库）
+ ios的符号化也主要是根据第5和第6部分进行符号化，只有把第五部分后面的二进制的地址信息映射成代码信息，才能发生crash的原因。
+ 
+ 下面是上面的crash日志符号化后的结果：
+ 
+
+ ### 1.进程信息 ###
+ Incident Identifier: 87164E05-84A8-40F2-886D-14F90C9D3F47
+ CrashReporter Key:   TODO
+ Hardware Model:      iPhone7,2
+ Process:         imeituan [945]
+ Path:            /var/mobile/Containers/Bundle/Application/96DC918D-60C8-4C49-A51D-C0D71D0FCB4D/imeituan.app/imeituan
+ Identifier:      com.meituan.imeituan
+ Version:         895
+ Code Type:       ARM-64
+ Parent Process:  ??? [1]
+ 
+ ### 2.基本信息 ###
+ Date/Time:       2016-02-01 16:14:24 +0000
+ OS Version:      iPhone OS 9.2.1 (13D15)
+ Report Version:  104
+ 
+ ### 3.异常信息 ###
+ Exception Type:  SIGABRT
+ Exception Codes: #0 at 0x181f3c140
+ 
+ ### 4.线程回溯 ###Crashed Thread:  0
+ Application Specific Information:
+ *** Terminating app due to uncaught exception 'NSRangeException', reason: '*** -[__NSArrayM objectAtIndex:]: index 25 beyond bounds [0 .. 24]'
+ 
+ ### 5.Crash调用堆栈，需要需要将堆栈的二进制转成可读的###
+ Last Exception Backtrace:
+ 0   CoreFoundation                      __exceptionPreprocess + 124
+ 1   libobjc.A.dylib                     objc_exception_throw + 56
+ 2   CoreFoundation                      -[__NSArrayM removeObjectAtIndex:] + 0
+ 3   imeituan                            -[SAKFetchedResultsController objectAtIndexPath:] (SAKFetchedResultsController.m:60)
+ 4   imeituan                            -[DEFHomePageViewController tableView:cellForRowAtIndexPath:] (DEFHomePageViewController.m:663)
+ 5   UIKit                               -[UITableView _createPreparedCellForGlobalRow:withIndexPath:willDisplay:] + 692
+ 6   UIKit                               -[UITableView _createPreparedCellForGlobalRow:willDisplay:] + 80
+ 7   UIKit                               -[UITableView _updateVisibleCellsNow:isRecursive:] + 2360
+ 8   UIKit                               -[UITableView _performWithCachedTraitCollection:] + 104
+ 9   UIKit                               -[UITableView layoutSubviews] + 176
+ 10  UIKit                               -[UIView(CALayerDelegate) layoutSublayersOfLayer:] + 656
+ 11  QuartzCore                          -[CALayer layoutSublayers] + 148
+ 12  QuartzCore                          CA::Layer::layout_if_needed(CA::Transaction*) + 292
+ 13  QuartzCore                          CA::Layer::layout_and_display_if_needed(CA::Transaction*) + 32
+ 14  QuartzCore                          CA::Context::commit_transaction(CA::Transaction*) + 252
+ 15  QuartzCore                          CA::Transaction::commit() + 512
+ 16  QuartzCore                          CA::Transaction::observer_callback(__CFRunLoopObserver*, unsigned long, void*) + 80
+ 17  CoreFoundation                      __CFRUNLOOP_IS_CALLING_OUT_TO_AN_OBSERVER_CALLBACK_FUNCTION__ + 32
+ 18  CoreFoundation                      __CFRunLoopDoObservers + 372
+ 19  CoreFoundation                      __CFRunLoopRun + 928
+ 20  CoreFoundation                      CFRunLoopRunSpecific + 384
+ 21  GraphicsServices                    GSEventRunModal + 180
+ 22  UIKit                               UIApplicationMain + 204
+ 23  imeituan                            main (main.m:34)
+ 24  ???                                 0x0000000181e1e8b8 0x0 + 0
+ ### 6.动态库信息 ###
+ Binary Images:
+ 0x1000b8000 -        0x10275ffff +imeituan arm64  <e38d01be571931b7a4c3d9dcbf28e821> /var/mobile/Containers/Bundle/Application/96DC918D-60C8-4C49-A51D-C0D71D0FCB4D/imeituan.app/imeituan
+ 0x10d76c000 -        0x10d7dbfff  AGXMetalG4P arm64  <f76b11f8d06338f99ae4704aba08111c> /System/Library/Extensions/AGXMetalG4P.bundle/AGXMetalG4P
+ 0x182058000 -        0x18225dfff  libicucore.A.dylib arm64  <5c1540546de5350ab314c1d4c8a46d1b> /usr/lib/libicucore.A.dylib
+ 
+ …
+ 0x182274000 -        0x1825ecfff  CoreFoundation arm64  <121118a9a44d3518b99f3ebfd8806f69> /System/Library/Frameworks/CoreFoundation.framework/CoreFoundation
+ 0x1825f0000 -
+ …
+
+ 
+ 
+ 
+ ///
+ 例如，我们用上面的例子，从上面原始日志的第5部分挑出crash的地址，崩溃问题的函数地址堆栈如下：
+ 
+ Last Exception Backtrace:
+ 0   CoreFoundation                      0x0000000182399900 0x182274000 + 1202432
+ 1   libobjc.A.dylib                     0x0000000181a07f80 0x181a00000 + 32640
+ 2   CoreFoundation                      0x000000018227f828 0x182274000 + 47144
+ 第一步：从第6部分找出所依赖的动态库的地址
+ 
+ “CoreFoundation”的动态库是“/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation”：
+ 
+ 0x182274000 -        0x1825ecfff  CoreFoundation arm64  <121118a9a44d3518b99f3ebfd8806f69> /System/Library/Frameworks/CoreFoundation.framework/CoreFoundation
+ 第二步：在符号表的该目录下验证uuid是不是对应上
+ 
+ 执行命令dwarfdump --uuid 来验证：
+ 
+ 执行命令：
+ dwarfdump --uuid /Users/jenkins/data/apps/OSSDKSymbols/9.2.1\ \(13D15\)/Symbols/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation
+ 
+ 输出：
+ UUID: 121118A9-A44D-3518-B99F-3EBFD8806F69 (arm64) /Users/jenkins/data/apps/OSSDKSymbols/9.2.1 (13D15)/Symbols/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation
+ UUID: 910B0F17-490F-3D92-9833-A9D4AADABBDB (armv7s) /Users/jenkins/data/apps/OSSDKSymbols/9.2.1 (13D15)/Symbols/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation
+ 发现“121118A9-A44D-3518-B99F-3EBFD8806F69”和“121118a9a44d3518b99f3ebfd8806f69”是吻合的（忽略大小写和‘－’），说明这个crash是在该版本中的。可以进行下面的符号化操作。
+ 
+ 第三步：用atos工具进行符号化
+ 
+ atos实际是一个可以把地址转换为函数名（包括行号）的工具，atos 语法：atos -o dysm文件路径 -l 模块load地址 调用方法的地址，上面的0x182274000就是模块load地，0x0000000182399900就是调用方法的地址。
+ 
+ 例如执行命令：
+ 
+ 执行命令：
+ atos -o /Users/jenkins/data/apps/OSSDKSymbols/9.2.1\ \(13D15\)/Symbols/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation -l 0x182274000 0x0000000182399900
+ 
+ 输出：
+ __exceptionPreprocess (in CoreFoundation) + 124
+ 发现输出的结果是：__exceptionPreprocess (in CoreFoundation) + 124，说明符号化成功
+ 
+ 至此符号就成功了，这个例子只是ios的os的crash日志符号化，还有就是app层面crash，上面全部的的符号化的结果都是根据这几个步骤来完成的。
+ 
+ 
+ 
+ 上面的符号化可以知道，我们需要"dwarfdump" 和 “atos” 命令，这是mac os 上带的符号化的工具，同时还依赖xcode的版本，这是ubantu和centos系统所不支持的，所以crash log的符号化必须是在mac os上进行的，所以crash符号化服务必须在mac os进行，物理机可以是mac mini、imac或macbook。
+ 
+ 至于上面crash业务服务，可以使用一般的ubantu或centos 系统都是可以的。
+ 
+ 那么问题来了，一般来说我们发布的服务都是在ubantu或是centos系统上的，怎么把服务部署在mac os机器上呢？其实都差不多，java因为有JVM，服务是跨平台的，只是需要Runtime调用本地的“dwarfdump" 和 “atos”命令。无非就是编译、打包、部署，当然了还有初始化机器，建立发布账号，建立路径，管理服务进程等繁琐的事情，这样就可以把java服务发布到远程的mac os机器上，问题就可以解决了。
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
